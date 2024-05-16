7e78170822c45a54192af0158f37f614b879bb82
shanae-H <sheslop@griddynamics.com>
2024-05-16 10:15:40 -0500

    created three more methods to reverse a string

--- ReverseString/ReverseString.java
+++ ReverseString/ReverseString.java
@@ -1,3 +1,6 @@
+import java.util.Collections;
+import java.util.Stack;
+
 public class ReverseString {
     private final String stringToRevers;
 
@@ -16,11 +19,46 @@ public class ReverseString {
         System.out.println(" ");
     }
 
+    public StringBuilder impl2(){
+        StringBuilder rString = new StringBuilder();
+        rString.append(stringToRevers);
+        rString.reverse();
+        return rString;
+    }
+    public void impl3(){
+        StringBuffer s= new StringBuffer();
+        s.append(stringToRevers);
+        s.reverse();
+        System.out.println(s);
+    }
+
+    public void impl4(){
+        Stack <Character> word= new Stack<Character>();
+        for(char c: stringToRevers.toCharArray()){
+            word.push(c);
+        }
+        String reversedString = "";
+        while (!word.isEmpty()){
+           System.out.print(word.pop());
+       }
+
+    }
     public static void main (String[] args){
         ReverseString rv= new ReverseString();
         rv.impl1();
 
         ReverseString obj = new ReverseString("I'm going to school tomorrow morning");
         obj.impl1();
+        
+        ReverseString obj2= new ReverseString("She sells sea shells by the seashore");
+        System.out.println(obj2.impl2());
+
+        ReverseString obj3= new ReverseString("Humpty Dumpty sat on a wall, Humpty Dumpty had a great fall");
+        obj3.impl3();
+
+        ReverseString obj4= new ReverseString("Does this even work?");
+        obj4.impl4();
+
+
     }
 }
